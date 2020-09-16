b14b21f87dabe22692095155b5cf2ca79731616e
Yulin <shin.yulin0516@gmail.com>
2020-09-16 22:29:37 +0800

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by yulin on 2020/9/16.
+//  Copyright © 2020 yulin.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
